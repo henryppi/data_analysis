8665385e0a63476fc43d8a7aa17f6b476366438a
Your Name <you@example.com>
2020-09-24 04:20:44 +0200

    added files

--- test_case_H_pressure/run_analyze_barometric_pressure.py
+++ test_case_H_pressure/run_analyze_barometric_pressure.py
@@ -1,2 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import requests
+
+
+url = 'https://raw.githubusercontent.com/henryppi/data_analysis/master/test_case_H_pressure/data_pressure/RawData1.csv'
+
+response = requests.get(url)
+text=response.iter_lines(decode_unicode='utf-8')
+data = np.genfromtxt(text,delimiter=',',skip_header=1)
+
+plt.plot(data[:,0],data[:,1])
+plt.show()
