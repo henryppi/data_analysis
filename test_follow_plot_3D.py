31211e3f76ec5e5351f462c47c6faf3c2e0a55a6
henryppi <none>
2026-09-17 21:34:45 +0200

    update

--- test_follow_plot_3D.py
+++ test_follow_plot_3D.py
@@ -5,15 +5,12 @@ import matplotlib.pyplot as plt
 
 
 # vert,elem = make_cube(2,1,0.5)
-vert,elem = make_ship(2,1,0.5)
+vert,elem = make_ship(2.5,1,0.6)
 
 cad = cad_object()
 cad.set_geom(vert,elem)
 cad.set_center_bbox()
 
-
 moni3D = Monitor3D(cad)
-
-
 moni3D.set_pose(0,0,0,0*np.pi/180,0,0)
 moni3D.update()
